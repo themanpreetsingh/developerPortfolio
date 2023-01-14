aa128d796518b6ded0bf3427371cea3639730427
themanpreetsingh <the.singh.manpreet5@gmail.com>
2023-01-14 23:06:16 +0530

    Updated Navbar Toggle Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
-let toggle = document.querySelector('.toggle');
+const toggleBtn = document.querySelector('.toggle');
+const closeBtn = document.querySelector('#close');
 
-// Toggle NavBar on small screens
-toggle.addEventListener('click', () =>
-{
+// Toggle NavBar on small screens 
+function toggleNav() {
     document.body.classList.toggle('header-visible');
-});
+    toggleBtn.classList.toggle('hide');
+    closeBtn.classList.toggle('hide');
+};
+
+toggleBtn.addEventListener('click', toggleNav);
+
+closeBtn.addEventListener('click', toggleNav);
 
 //TODO: highlight the section name in nav bar for the open section
