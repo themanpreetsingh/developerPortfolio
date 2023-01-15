e2c77a6af8f4414fcedea4ced33fb9d8d93f5cfa
themanpreetsingh <the.singh.manpreet5@gmail.com>
2023-01-15 23:12:31 +0530

    Navbar Link Highlighting - Click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const toggleBtn = document.querySelector('.toggle');
 const closeBtn = document.querySelector('#close');
+const navBarLinks = Array.from(document.querySelectorAll('#nav a'));
 
 // Toggle NavBar on small screens 
 function toggleNav() {
@@ -13,3 +14,12 @@ toggleBtn.addEventListener('click', toggleNav);
 closeBtn.addEventListener('click', toggleNav);
 
 //TODO: highlight the section name in nav bar for the open section
+
+
+// Highlight the link clicked by user
+navBarLinks.forEach( link => link.addEventListener('click', (eventDetails) => switchActiveLink(eventDetails.target)));
+function switchActiveLink(selectedLink) {
+    const currentlyActive = navBarLinks.filter( link => link.classList.contains('active') )[0]; 
+    currentlyActive.classList.remove('active');
+    selectedLink.classList.add('active');
+}
