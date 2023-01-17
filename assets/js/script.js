5e1407cbec0bf604b13bb8a1ecafdae384cc14ef
themanpreetsingh <the.singh.manpreet5@gmail.com>
2023-01-17 15:58:14 +0530

    Highlight the section name in nav bar for the open section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 const toggleBtn = document.querySelector('.toggle');
 const closeBtn = document.querySelector('#close');
 const navBarLinks = Array.from(document.querySelectorAll('#nav a'));
+const sections = Array.from(document.querySelectorAll('#main section'));
 
-// Toggle NavBar on small screens 
+/** Toggle NavBar on small screens **/
 function toggleNav() {
     document.body.classList.toggle('header-visible');
     toggleBtn.classList.toggle('hide');
@@ -13,8 +14,33 @@ toggleBtn.addEventListener('click', toggleNav);
 
 closeBtn.addEventListener('click', toggleNav);
 
-//TODO: highlight the section name in nav bar for the open section
+/**Highlight the section name in nav bar for the open section**/
 
+    //Takes a section as input and returns the corresponding link in navBar
+    function getLinkForSection(section) {
+        return navBarLinks.filter(link => link.getAttribute("href") === ("#" + section.getAttribute("id")))[0];
+    }
+
+    //options for observer 
+    const options = {
+        threshold: 0.5
+    }
+
+    //callback function for observer
+    const callback = (entries, observer) => {
+        entries.filter(entry => entry.isIntersecting && entry.intersectionRatio >= 0.5)
+        .forEach(entry => {
+            const link = getLinkForSection(entry.target);
+            switchActiveLink(link);
+        });
+    }
+    const observer = new IntersectionObserver(callback, options);
+
+    //observe every section in the main div
+    sections.forEach(section => {
+        observer.observe(section);
+    })
+  
 
 // Highlight the link clicked by user
 navBarLinks.forEach( link => link.addEventListener('click', (eventDetails) => switchActiveLink(eventDetails.target)));
